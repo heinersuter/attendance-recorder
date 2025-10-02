03041d3e42cd3704f7019b5e812d24654639ff11
Heiner Suter <heiner.suter@zuehlke.com>
2025-10-02 12:46:42 +0200

    Use a folder per year

--- AttendanceRecorder.LifeSign/LifeSignWriter.cs
+++ AttendanceRecorder.LifeSign/LifeSignWriter.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.Extensions.Options;
-
-namespace AttendanceRecorder.LifeSign;
+﻿namespace AttendanceRecorder.LifeSign;
 
 public class LifeSignWriter(LifeSignConfig config)
 {
     public void WriteLifeSign()
     {
-        Directory.CreateDirectory(config.Directory);
-
         var now = DateTime.Now;
 
-        var filePath = Path.Combine(config.Directory, $"{now:yyyy-MM-dd}.attrec");
+        var yearDirectory = Path.Combine(config.Directory, $"{now.Year}");
+        Directory.CreateDirectory(yearDirectory);
+
+        var filePath = Path.Combine(yearDirectory, $"{now:MM-dd}.attrec");
 
         File.AppendAllText(filePath, $"{now:HH:mm:ss}{Environment.NewLine}");
     }
